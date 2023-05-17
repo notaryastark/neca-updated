9a1cd93315cff28caf1fdef19cc50f43bf4c8577
notaryastark <95669586+notaryastark@users.noreply.github.com>
2023-05-17 15:11:34 +0545

    Initial commit for NECA

---
+++ src/script.js
@@ -0,0 +1,12 @@
+import "alpinejs";
+
+// $(document).ready(function () {
+//     $(".nav-toggler").each(function (_, navToggler) {
+//       var target = $(navToggler).data("target");
+//       $(navToggler).on("click", function () {
+//         $(target).animate({
+//           height: "toggle",
+//         });
+//       });
+//     });
+//   });
