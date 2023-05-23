f0ac012920cff50cd56f621f4a60044e7fd4ea25
notaryastark <95669586+notaryastark@users.noreply.github.com>
2023-05-23 15:22:33 +0545

    folder changes

--- src/script.js
+++ src/script.js
@@ -1,5 +1,19 @@
 import "alpinejs";
 
+var menu = document.getElementById("menu");
+    // this function is used to open the menu
+    function openMenu() {
+        menu.classList.remove("w-0", "h-0", "opacity-0");
+        menu.classList.add("w-screen", "h-screen", "opacity-95");
+    }
+
+    // this function is used to close the menu
+    function closeMenu() {
+        menu.classList.remove("w-screen", "h-screen", "opactiy-95");
+        menu.classList.add("w-0", "h-0", "opacity-0");
+    }
+
+
 // $(document).ready(function () {
 //     $(".nav-toggler").each(function (_, navToggler) {
 //       var target = $(navToggler).data("target");
